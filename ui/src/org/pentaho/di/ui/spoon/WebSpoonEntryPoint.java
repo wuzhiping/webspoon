6579819d2cdc967c0a86363fbecf86d02c638ecf
Hiromu Hota <hiromu.hota@hal.hitachi.com>
2017-02-20 14:01:43 -0800

    Rework: Take parameters in url to open a file
    It is a rework of fe35ff3f9da6a7573a6144732c084a78941adf52.
    The query format is now the same as Pan/Kitchen options, described in https://help.pentaho.com/Documentation/5.1/0L0/0Y0/0J0/010/010,
    except for that only some of the options ("rep", "user", "pass", "trans", "job", "dir", "file") can be used.

--- ui/src/org/pentaho/di/ui/spoon/WebSpoonEntryPoint.java
+++ ui/src/org/pentaho/di/ui/spoon/WebSpoonEntryPoint.java
@@ -24,26 +24,21 @@
 package org.pentaho.di.ui.spoon;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import org.eclipse.rap.rwt.RWT;
 import org.eclipse.rap.rwt.application.AbstractEntryPoint;
 import org.eclipse.rap.rwt.client.service.ExitConfirmation;
 import org.eclipse.rap.rwt.client.service.StartupParameters;
-import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.MessageBox;
-import org.pentaho.di.core.LastUsedFile;
 import org.pentaho.di.core.Props;
 import org.pentaho.di.core.logging.KettleLogStore;
 import org.pentaho.di.ui.core.PropsUI;
-import org.springframework.util.CollectionUtils;
 
 public class WebSpoonEntryPoint extends AbstractEntryPoint {
 
   @Override
-  protected void createContents(Composite parent) {
+  protected void createContents( Composite parent ) {
     /*
      *  The following lines were migrated from Spoon.main
      *  because they are session specific.
@@ -52,8 +47,18 @@ public class WebSpoonEntryPoint extends AbstractEntryPoint {
     KettleLogStore
       .init( PropsUI.getInstance().getMaxNrLinesInLog(), PropsUI.getInstance().getMaxLogLineTimeoutMinutes() );
 
+    // Options
+    StartupParameters serviceParams = RWT.getClient().getService( StartupParameters.class );
+    List<String> args = new ArrayList<String>();
+    String[] options = { "rep", "user", "pass", "trans", "job", "dir", "file" };
+    for ( String option : options ) {
+      if ( serviceParams.getParameter( option ) != null ) {
+        args.add( "-" + option + "=" + serviceParams.getParameter( option ) );
+      }
+    }
+
     // Execute Spoon.createContents
-    Spoon.getInstance().setCommandLineArgs( Spoon.getCommandLineArgs( new ArrayList<String>( Arrays.asList( "" ) ) ) );
+    Spoon.getInstance().setCommandLineArgs( Spoon.getCommandLineArgs( args ) );
     Spoon.getInstance().setShell( parent.getShell() );
     Spoon.getInstance().createContents( parent );
 
@@ -62,29 +67,6 @@ public class WebSpoonEntryPoint extends AbstractEntryPoint {
      */
     ExitConfirmation serviceConfirm = RWT.getClient().getService( ExitConfirmation.class );
     serviceConfirm.setMessage( "Do you really wanna leave this site?" );
-
-    /* Open a file when appropriate parameters are given */
-    StartupParameters serviceParams = RWT.getClient().getService( StartupParameters.class );
-    String fileType = serviceParams.getParameter( "fileType" );
-    String filename = serviceParams.getParameter( "filename" );
-    String directory = serviceParams.getParameter( "directory" );
-    Boolean sourceRepository = serviceParams.getParameter( "sourceRepository" ) == null ? null : serviceParams.getParameter( "sourceRepository" ).equalsIgnoreCase( "true" );
-    String repositoryName = serviceParams.getParameter( "repositoryName" );
-    if ( !CollectionUtils.isEmpty( serviceParams.getParameterNames() ) ) {
-      if ( fileType == null | filename == null | directory == null | sourceRepository == null | repositoryName == null ) {
-        MessageBox box = new MessageBox( parent.getShell(), SWT.ICON_WARNING | SWT.OK );
-        box.setText( "Incorrect parameters" );
-        box.setMessage( "Check your URL parameters\n"
-          + "Ex1) /?fileType=Trans\n&&filename=Transformation%201\n&&directory=%2Fhome%2Fadmin\n&&sourceRepository=true\n&&repositoryName=pentaho-ee\n"
-          + "Ex2) /?fileType=Trans\n&&filename=file%3A%2F%2F%2Fhome%2Fuser%2F%2FTransformation%202.ktr\n&&directory=\n&&sourceRepository=false\n&&repositoryName=" );
-        box.open();
-      } else {
-        List<LastUsedFile> lastUsedFiles = new ArrayList<LastUsedFile>();
-        lastUsedFiles.add( new LastUsedFile( fileType, filename, directory, sourceRepository, repositoryName, false, LastUsedFile.OPENED_ITEM_TYPE_MASK_GRAPH ) );
-        Spoon.getInstance().props.setLastUsedFiles( lastUsedFiles );
-        Spoon.getInstance().lastFileSelect( "0" );
-      }
-    }
   }
 
 }

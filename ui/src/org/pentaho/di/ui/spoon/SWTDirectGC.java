2f2e79dd4321798a74ee8004c00fb7a93faf5cab
Hiromu Hota <hiromu.hota@hal.hitachi.com>
2017-02-23 14:23:33 -0800

    Restore the use of LineStyle and LineAttributes

--- ui/src/org/pentaho/di/ui/spoon/SWTDirectGC.java
+++ ui/src/org/pentaho/di/ui/spoon/SWTDirectGC.java
@@ -365,7 +365,7 @@ public class SWTDirectGC implements GCInterface {
         break;
       case PARALLEL:
         gc.setLineAttributes( new LineAttributes(
-          gc.getLineWidth(), SWT.CAP_FLAT, SWT.JOIN_MITER ) );
+          gc.getLineWidth(), SWT.CAP_FLAT, SWT.JOIN_MITER, SWT.LINE_CUSTOM, new float[] { 5, 3, }, 0, 10 ) );
         break;
       default:
         break;
--- ui/src/org/pentaho/di/ui/spoon/SWTGC.java
+++ ui/src/org/pentaho/di/ui/spoon/SWTGC.java
@@ -351,6 +351,26 @@ public class SWTGC implements GCInterface {
   }
 
   public void setLineStyle( ELineStyle lineStyle ) {
+    switch ( lineStyle ) {
+      case DASHDOT:
+        gc.setLineStyle( SWT.LINE_DASHDOT );
+        break;
+      case SOLID:
+        gc.setLineStyle( SWT.LINE_SOLID );
+        break;
+      case DOT:
+        gc.setLineStyle( SWT.LINE_DOT );
+        break;
+      case DASH:
+        gc.setLineStyle( SWT.LINE_DASH );
+        break;
+      case PARALLEL:
+        gc.setLineAttributes( new LineAttributes(
+          gc.getLineWidth(), SWT.CAP_FLAT, SWT.JOIN_MITER, SWT.LINE_CUSTOM, new float[] { 5, 3, }, 0, 10 ) );
+        break;
+      default:
+        break;
+    }
   }
 
   public void setLineWidth( int width ) {

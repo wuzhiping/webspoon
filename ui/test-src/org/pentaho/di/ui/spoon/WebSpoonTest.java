wuzhiping/webspoon

commit 94f07bff4e1019fc187333a43b9c9530eaa0fd59

Set location of the context menu properly

ui/src/org/pentaho/di/ui/core/ConstUI.java
@@ -261,15 +261,22 @@ public class ConstUI {
   public static void displayMenu( Menu menu, Control control ) {
     Menu oldMenu = control.getMenu();
     if ( oldMenu != null && oldMenu != menu ) {
-      oldMenu.setVisible( false );
+//      oldMenu.setVisible( false );
+      oldMenu.dispose();
     }
 
     // XXX: Stubbing out this line prevents context dialogs from appearing twice
     // on OS X. Tested on Windows to be sure there is no adverse effect.
     // Unfortunately, I do *not* understand why this works. I ran it by
     // mcasters and he didn't know for sure either.
-    // control.setMenu(menu);
-    menu.setVisible( true );
+    /*
+     * This dates back to May 22, 2008 (b065e5d6be6bb93e019dc127077843144f7e03da).
+     * PDI-1272: Context menus get duplicated when right-clicking on Trans/Job canvas in OS X
+     * However, RWT does not depend on OS, so dialogs appear once even when a server is running on OS X.
+     * I revert back to control.setMenu from menu.setVisible for webSpoon.
+     */
+    control.setMenu( menu );
+//    menu.setVisible( true );
   }
 
 }

commit 270fe107b8c086cba953596c57e267aa7eaaf583

Fix the regression caused by 94f07bff4e1019fc187333a43b9c9530eaa0fd59
The regression is that the context menu of the tree in the View tab won't pop-up.

ui/src/org/pentaho/di/ui/core/ConstUI.java
@@ -259,11 +259,15 @@ public class ConstUI {
   }
 
   public static void displayMenu( Menu menu, Control control ) {
-    Menu oldMenu = control.getMenu();
-    if ( oldMenu != null && oldMenu != menu ) {
+    /*
+     * Comment-out these lines to resolve the regression caused by 94f07bff4e1019fc187333a43b9c9530eaa0fd59
+     * The regression is that the context menu of the tree in the View tab won't pop-up.
+     */
+//    Menu oldMenu = control.getMenu();
+//    if ( oldMenu != null && oldMenu != menu ) {
 //      oldMenu.setVisible( false );
-      oldMenu.dispose();
-    }
+//      oldMenu.dispose();
+//    }
 
     // XXX: Stubbing out this line prevents context dialogs from appearing twice
     // on OS X. Tested on Windows to be sure there is no adverse effect.

ui/test-src/org/pentaho/di/ui/spoon/WebSpoonTest.java
@@ -155,6 +155,29 @@ public class WebSpoonTest {
     Assert.assertEquals( "5", driver.switchTo().activeElement().getAttribute( "tabindex" ) );
   }
 
+  @Test
+  public void testContextMenu() throws Exception {
+    // Create a new transformation
+    createNewTrans();
+    drawStep( "Table input" );
+
+    // Open a step dialog
+    clickElement( "//div[@test-id = 'tree_exploreSolution']" );
+    clickElement( "//div[@test-id = 'tree_expandAll']" );
+
+    // Right-click on the step to pop-up a context menu
+    // for some reason, multiple double right clicks are needed.
+    for ( int i = 0; i < 5; i++ ){
+      element = wait.until( ExpectedConditions.elementToBeClickable( By.xpath( "//div[@test-id = 'tree_Steps']/../..//div[text() = 'Table input']" ) ) );
+      actions.contextClick( element ).build().perform();
+      if ( driver.findElements( By.xpath( "//div[text() = 'Duplicate']" ) ).size() == 1 ) {
+        break;
+      }
+    }
+
+    Assert.assertEquals( 1, driver.findElements( By.xpath( "//div[text() = 'Duplicate']" ) ).size() );
+  }
+
   private void createNewTrans() {
     // Create a new transformation
     clickElement( "//div[text() = 'File']" );
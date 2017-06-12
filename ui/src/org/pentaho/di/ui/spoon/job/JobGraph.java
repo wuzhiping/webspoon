wuzhiping/webspoon

commit 3fd313329e38be2cc0955dfa68fd7bb446806bca

Fix step/job entry manipulations
- A step/job entry can be moved without explicitly selecting it.
- A step can be copied and pasted directly after D&D.
(This is not the case for a job entry because the entry is not selected after D&D)

ui/src/org/pentaho/di/ui/spoon/job/JobGraph.java
@@ -495,6 +495,7 @@ public class JobGraph extends AbstractGraph implements XulEventHandler, Redrawab
         }
 
         Point p = getRealPosition( canvas, event.x, event.y );
+        lastMove = p;
 
         try {
           DragAndDropContainer container = (DragAndDropContainer) event.data;
@@ -847,6 +848,7 @@ public class JobGraph extends AbstractGraph implements XulEventHandler, Redrawab
         }
       }
     }
+    mouseMove( e );
   }
 
   public void mouseUp( MouseEvent e ) {

ui/src/org/pentaho/di/ui/spoon/trans/TransGraph.java
@@ -629,6 +629,7 @@ public class TransGraph extends AbstractGraph implements XulEventHandler, Redraw
 
         // What's the real drop position?
         Point p = getRealPosition( canvas, event.x, event.y );
+        lastMove = p;
 
         //
         // We expect a Drag and Drop container... (encased in XML)
@@ -1047,6 +1048,7 @@ public class TransGraph extends AbstractGraph implements XulEventHandler, Redraw
         }
       }
     }
+    mouseMove( e );
   }
 
   @Override

commit 033fa2af1c950d8a3fee544d5726b6118337c5fb

Resolve a regression, caused by 3fd31332, that the mouse-over information is not displayed when there is only one step/job entry

ui/src/org/pentaho/di/ui/spoon/job/JobGraph.java
@@ -1180,7 +1180,7 @@ public class JobGraph extends AbstractGraph implements XulEventHandler, Redrawab
         for ( int i = 0; i < selectedEntries.size(); i++ ) {
           JobEntryCopy jobEntryCopy = selectedEntries.get( i );
           PropsUI.setLocation( jobEntryCopy, jobEntryCopy.getLocation().x + dx, jobEntryCopy.getLocation().y + dy );
-          stopEntryMouseOverDelayTimer( jobEntryCopy );
+//          stopEntryMouseOverDelayTimer( jobEntryCopy );
         }
       }
       // Adjust location of selected hops...

ui/src/org/pentaho/di/ui/spoon/trans/TransGraph.java
@@ -1450,7 +1450,7 @@ public class TransGraph extends AbstractGraph implements XulEventHandler, Redraw
         for ( int i = 0; i < selectedSteps.size(); i++ ) {
           StepMeta stepMeta = selectedSteps.get( i );
           PropsUI.setLocation( stepMeta, stepMeta.getLocation().x + dx, stepMeta.getLocation().y + dy );
-          stopStepMouseOverDelayTimer( stepMeta );
+//          stopStepMouseOverDelayTimer( stepMeta );
         }
       }
       // Adjust location of selected hops...
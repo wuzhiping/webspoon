wuzhiping/webspoon

commit da920b63f00fa38761729bae3ac3f94d8f3f5559

FileDialog.open() should have returned an absolute path

ui/src/org/eclipse/swt/widgets/FileDialog.java
@@ -84,7 +84,7 @@ public class FileDialog implements IFileDialog {
       } catch ( FileSystemException e ) {
         e.printStackTrace();
       }
-      filePath = fileName = fileNames[0] = file.getName();
+      filePath = fileName = fileNames[0] = file.getAbsolutePath();
       filterPath = file.getParent();
     }
     return filePath;

commit 48ced04c430c0a4132467dda728a9ac6e6cfada7

fileName should be a relative path to the filterPath
This is a regression from da920b63f00fa38761729bae3ac3f94d8f3f5559

ui/src/org/eclipse/swt/widgets/FileDialog.java
@@ -84,7 +84,8 @@ public class FileDialog implements IFileDialog {
       } catch ( FileSystemException e ) {
         e.printStackTrace();
       }
-      filePath = fileName = fileNames[0] = file.getAbsolutePath();
+      filePath = file.getAbsolutePath();
+      fileName = fileNames[0] = file.getName();
       filterPath = file.getParent();
     }
     return filePath;
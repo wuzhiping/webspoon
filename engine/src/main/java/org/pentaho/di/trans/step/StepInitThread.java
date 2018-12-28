wuzhiping/webspoon

commit 5d364b571a9cfc90900781fc214d0f2d627b1d5e

Execute StepInitThread and TransGraph.prepareTrans in the context of a UI session (#110)

engine/src/main/java/org/pentaho/di/trans/step/StepInitThread.java
@@ -22,6 +22,8 @@
 
 package org.pentaho.di.trans.step;
 
+import org.eclipse.rap.rwt.RWT;
+import org.eclipse.rap.rwt.service.UISession;
 import org.pentaho.di.core.Const;
 import org.pentaho.di.core.logging.LogChannelInterface;
 import org.pentaho.di.core.logging.Metrics;
@@ -40,12 +42,15 @@ public class StepInitThread implements Runnable {
 
   private LogChannelInterface log;
 
+  final private UISession session;
+
   public StepInitThread( StepMetaDataCombi combi, LogChannelInterface log ) {
     this.combi = combi;
     this.log = combi.step.getLogChannel();
     this.ok = false;
     this.finished = false;
     this.doIt = true;
+    this.session = RWT.getUISession();
   }
 
   public String toString() {
@@ -53,6 +58,7 @@ public class StepInitThread implements Runnable {
   }
 
   public void run() {
+    session.exec( () -> {
     // Set the internal variables also on the initialization thread!
     // ((BaseStep)combi.step).setInternalVariables();
 
@@ -81,6 +87,7 @@ public class StepInitThread implements Runnable {
     }
 
     finished = true;
+    });
   }
 
   public boolean isFinished() {

ui/src/main/java/org/pentaho/di/ui/spoon/trans/TransGraph.java
@@ -34,6 +34,7 @@ import org.eclipse.rap.rwt.RWT;
 import org.eclipse.jface.window.DefaultToolTip;
 import org.eclipse.jface.window.ToolTip;
 import org.eclipse.rap.rwt.scripting.ClientListener;
+import org.eclipse.rap.rwt.service.UISession;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.CTabFolder;
 import org.eclipse.swt.custom.CTabItem;
@@ -4426,9 +4427,10 @@ public class TransGraph extends AbstractGraph implements XulEventHandler, Redraw
   private synchronized void prepareTrans( final Thread parentThread, final String[] args ) {
     pushSession.start();
     trans.setDisplay( Display.getCurrent() );
-    Runnable runnable = new Runnable() {
-      @Override
-      public void run() {
+    final Display display = Display.getCurrent();
+    Runnable runnable = () -> {
+      UISession uiSession = RWT.getUISession( display );
+      uiSession.exec( () -> {
         try {
           trans.prepareExecution( args );
 
@@ -4451,7 +4453,7 @@ public class TransGraph extends AbstractGraph implements XulEventHandler, Redraw
           running = false;
           checkErrorVisuals();
         }
-      }
+      });
     };
     Thread thread = new Thread( runnable );
     thread.start();

commit 4ab2d0643862a84e227c145beb20870cd466b67f

Fixed test failures caused by 5d364b57

engine/src/main/java/org/pentaho/di/trans/step/StepInitThread.java
@@ -42,7 +42,7 @@ public class StepInitThread implements Runnable {
 
   private LogChannelInterface log;
 
-  final private UISession session;
+  private UISession uiSession;
 
   public StepInitThread( StepMetaDataCombi combi, LogChannelInterface log ) {
     this.combi = combi;
@@ -50,7 +50,11 @@ public class StepInitThread implements Runnable {
     this.ok = false;
     this.finished = false;
     this.doIt = true;
-    this.session = RWT.getUISession();
+    try {
+      this.uiSession = RWT.getUISession();
+    } catch ( Exception e ) {
+      this.uiSession = null;
+    }
   }
 
   public String toString() {
@@ -58,7 +62,6 @@ public class StepInitThread implements Runnable {
   }
 
   public void run() {
-    session.exec( () -> {
     // Set the internal variables also on the initialization thread!
     // ((BaseStep)combi.step).setInternalVariables();
 
@@ -72,12 +75,24 @@ public class StepInitThread implements Runnable {
     try {
       combi.step.getLogChannel().snap( Metrics.METRIC_STEP_INIT_START );
 
-      if ( combi.step.init( combi.meta, combi.data ) ) {
-        combi.data.setStatus( StepExecutionStatus.STATUS_IDLE );
-        ok = true;
+      if ( uiSession == null ) {
+        if ( combi.step.init( combi.meta, combi.data ) ) {
+          combi.data.setStatus( StepExecutionStatus.STATUS_IDLE );
+          ok = true;
+        } else {
+          combi.step.setErrors( 1 );
+          log.logError( BaseMessages.getString( PKG, "Trans.Log.ErrorInitializingStep", combi.step.getStepname() ) );
+        }
       } else {
-        combi.step.setErrors( 1 );
-        log.logError( BaseMessages.getString( PKG, "Trans.Log.ErrorInitializingStep", combi.step.getStepname() ) );
+        uiSession.exec( () -> {
+          if ( combi.step.init( combi.meta, combi.data ) ) {
+            combi.data.setStatus( StepExecutionStatus.STATUS_IDLE );
+            ok = true;
+          } else {
+            combi.step.setErrors( 1 );
+            log.logError( BaseMessages.getString( PKG, "Trans.Log.ErrorInitializingStep", combi.step.getStepname() ) );
+          }
+        });
       }
     } catch ( Throwable e ) {
       log.logError( BaseMessages.getString( PKG, "Trans.Log.ErrorInitializingStep", combi.step.getStepname() ) );
@@ -87,7 +102,6 @@ public class StepInitThread implements Runnable {
     }
 
     finished = true;
-    });
   }
 
   public boolean isFinished() {
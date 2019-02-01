wuzhiping/webspoon

commit 34029c0d9d1ab73062582074d49d52c0fd946cde

Let Const.getUser() work if cid is available (fix #147)
Change the package of `WebSpoonUtils` from /ui to /core and its namespace.

ui/src/main/java/org/pentaho/di/ui/spoon/WebSpoonEntryPoint.java
@@ -37,6 +37,7 @@ import org.pentaho.di.core.Const;
 import org.pentaho.di.core.KettleClientEnvironment;
 import org.pentaho.di.core.LastUsedFile;
 import org.pentaho.di.core.Props;
+import org.pentaho.di.core.WebSpoonUtils;
 import org.pentaho.di.core.extension.ExtensionPointHandler;
 import org.pentaho.di.core.extension.KettleExtensionPoint;
 import org.pentaho.di.core.logging.LogChannel;
@@ -55,6 +56,7 @@ public class WebSpoonEntryPoint extends AbstractEntryPoint {
     // Set UISession so that any child thread of UIThread can access it
     WebSpoonUtils.setUISession( RWT.getUISession() );
     WebSpoonUtils.setUISession( WebSpoonUtils.getConnectionId(), RWT.getUISession() );
+    WebSpoonUtils.setUser( WebSpoonUtils.getConnectionId(), RWT.getRequest().getRemoteUser() );
     // Transferring Widget Data for client-side canvas drawing instructions
     WidgetUtil.registerDataKeys( "props" );
     WidgetUtil.registerDataKeys( "mode" );
@@ -117,6 +119,7 @@ public class WebSpoonEntryPoint extends AbstractEntryPoint {
          *  But the one at WebSpoonUtils.uiSessionMap should be explicitly removed.
          */
         WebSpoonUtils.removeUISession( WebSpoonUtils.getConnectionId() );
+        WebSpoonUtils.removeUser( WebSpoonUtils.getConnectionId() );
         Spoon.getInstance().quitFile( false );
       } catch ( Exception e ) {
         LogChannel.GENERAL.logError( "Error closing Spoon", e );

core/src/main/java/org/pentaho/di/core/Const.java
@@ -26,7 +26,6 @@ package org.pentaho.di.core;
 import org.apache.commons.lang.StringEscapeUtils;
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang.text.StrBuilder;
-import org.eclipse.rap.rwt.RWT;
 import org.pentaho.di.core.exception.KettleException;
 import org.pentaho.di.core.row.ValueMetaInterface;
 import org.pentaho.di.core.util.EnvUtil;
@@ -2035,7 +2034,7 @@ public class Const {
 
   public static String getUser() {
     try {
-      return RWT.getRequest().getRemoteUser();
+      return WebSpoonUtils.getUser( WebSpoonUtils.getConnectionId() );
     } catch ( Exception e ) { // when accessed from background threads (e.g., when the webSpoon server is starting)
       return null;
     }

core/src/main/java/org/pentaho/di/core/WebSpoonUtils.java
@@ -20,7 +20,7 @@
  *
  ******************************************************************************/
 
-package org.pentaho.di.ui.spoon;
+package org.pentaho.di.core;
 
 import java.util.HashMap;
 
@@ -37,6 +37,10 @@ public class WebSpoonUtils {
 
   private static final InheritableThreadLocal<UISession> uiSession = new InheritableThreadLocal<UISession>();
   private static final HashMap<String, UISession> uiSessionMap = new HashMap<String, UISession>();
+  /**
+   * This is a map between connectionId (cid) and user.
+   */
+  private static final HashMap<String, String> userMap = new HashMap<String, String>();
 
   public static void setTestId( Widget widget, String value ) {
     if ( !widget.isDisposed() ) {
@@ -99,6 +103,18 @@ public class WebSpoonUtils {
 
   public static void removeUISession( String cid ) {
     uiSessionMap.remove( cid );
+  }
+
+  public static void setUser( String cid, String user ) {
+    userMap.put( cid, user );
+  }
+
+  public static String getUser( String cid ) {
+    return userMap.get( cid );
+  }
+
+  public static void removeUser( String cid ) {
+    userMap.remove( cid );
   }
 
   public static String getConnectionId() {

plugins/file-open-save/core/pom.xml
@@ -24,11 +24,11 @@
     <dependency>
       <groupId>pentaho-kettle</groupId>
       <artifactId>kettle-core</artifactId>
+      <version>${webspoon.version}</version>
     </dependency>
     <dependency>
       <groupId>pentaho-kettle</groupId>
       <artifactId>kettle-ui-swt</artifactId>
-      <version>${webspoon.version}</version>
     </dependency>
     <dependency>
       <groupId>pentaho-kettle</groupId>

plugins/file-open-save/core/src/main/java/org/pentaho/repo/endpoint/WebSpoonFilter.java
@@ -31,7 +31,7 @@ import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.ext.Provider;
 
 import org.eclipse.rap.rwt.service.UISession;
-import org.pentaho.di.ui.spoon.WebSpoonUtils;
+import org.pentaho.di.core.WebSpoonUtils;
 
 @Provider
 public class WebSpoonFilter implements ContainerRequestFilter {

plugins/repositories/core/pom.xml
@@ -84,13 +84,13 @@
     <dependency>
       <groupId>pentaho-kettle</groupId>
       <artifactId>kettle-core</artifactId>
-      <version>${pdi.version}</version>
+      <version>${webspoon.version}</version>
       <scope>provided</scope>
     </dependency>
     <dependency>
       <groupId>pentaho-kettle</groupId>
       <artifactId>kettle-ui-swt</artifactId>
-      <version>${webspoon.version}</version>
+      <version>${pdi.version}</version>
       <scope>provided</scope>
     </dependency>
     <dependency>

plugins/repositories/core/src/main/java/org/pentaho/di/ui/repo/endpoints/WebSpoonFilter.java
@@ -31,7 +31,7 @@ import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.ext.Provider;
 
 import org.eclipse.rap.rwt.service.UISession;
-import org.pentaho.di.ui.spoon.WebSpoonUtils;
+import org.pentaho.di.core.WebSpoonUtils;
 
 @Provider
 public class WebSpoonFilter implements ContainerRequestFilter {

plugins/repositories/core/src/main/java/org/pentaho/di/ui/repo/model/UserRepositoriesMeta.java
@@ -24,10 +24,10 @@
 package org.pentaho.di.ui.repo.model;
 
 import org.eclipse.rap.rwt.SingletonUtil;
+import org.pentaho.di.core.WebSpoonUtils;
 import org.pentaho.di.core.exception.KettleException;
 import org.pentaho.di.repository.RepositoriesMeta;
 import org.pentaho.di.repository.RepositoryMeta;
-import org.pentaho.di.ui.spoon.WebSpoonUtils;
 
 public class UserRepositoriesMeta {
   private RepositoryMeta currentRepository;

commit 5bf02464786b416b1e86953be7f2d03fbf6b9f99

Fix a regression caused by 34029c0d

ui/src/main/java/org/pentaho/di/ui/spoon/WebSpoonEntryPoint.java
@@ -51,7 +51,7 @@ public class WebSpoonEntryPoint extends AbstractEntryPoint {
   protected void createContents( Composite parent ) {
     SecurityManager securityManager = System.getSecurityManager();
     if ( securityManager instanceof WebSpoonSecurityManager ) {
-      ( (WebSpoonSecurityManager) securityManager ).setUserName( Const.getUser() );
+      ( (WebSpoonSecurityManager) securityManager ).setUserName( RWT.getRequest().getRemoteUser() );
     }
     // Set UISession so that any child thread of UIThread can access it
     WebSpoonUtils.setUISession( RWT.getUISession() );
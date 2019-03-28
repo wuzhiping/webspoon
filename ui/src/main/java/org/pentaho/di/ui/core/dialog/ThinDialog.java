wuzhiping/webspoon

commit a55b60e98aca47da6ff71ca5c2ee2339c79936ac

Put RepositoryEndpoint in the context of UISession

ui/src/main/java/org/pentaho/di/ui/core/dialog/ThinDialog.java
@@ -22,10 +22,14 @@
 
 package org.pentaho.di.ui.core.dialog;
 
+import org.eclipse.rap.rwt.service.ServerPushSession;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.browser.Browser;
+import org.eclipse.swt.browser.BrowserFunction;
 import org.eclipse.swt.browser.CloseWindowListener;
 import org.eclipse.swt.browser.WindowEvent;
+import org.eclipse.swt.events.DisposeEvent;
+import org.eclipse.swt.events.DisposeListener;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.graphics.Rectangle;
@@ -34,6 +38,7 @@ import org.eclipse.swt.widgets.Dialog;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.MessageBox;
 import org.eclipse.swt.widgets.Shell;
+import org.pentaho.di.ui.spoon.WebSpoonUtils;
 
 /**
  * Created by bmorrise on 2/18/16.
@@ -76,6 +81,11 @@ public class ThinDialog extends Dialog {
           shell.close();
         }
       } );
+      new BrowserFunction( browser, "getConnectionId" ) {
+        @Override public Object function( Object[] arguments ) {
+          return WebSpoonUtils.getConnectionId();
+        }
+      };
     } catch ( Exception e ) {
       MessageBox messageBox = new MessageBox( dialog, SWT.ICON_ERROR | SWT.OK );
       messageBox.setMessage( "Browser cannot be initialized." );
@@ -83,6 +93,11 @@ public class ThinDialog extends Dialog {
       messageBox.open();
     }
     setPosition();
+    final ServerPushSession pushSession = new ServerPushSession();
+    pushSession.start();
+    dialog.addDisposeListener( ( event ) -> {
+      pushSession.stop();
+    });
     dialog.open();
   }
 

plugins/repositories/core/pom.xml
@@ -16,7 +16,6 @@
   </parent>
 
   <properties>
-    <fasterxml.jackson.version>2.9.2</fasterxml.jackson.version>
     <pdi.version>8.2.0.0-342</pdi.version>
     <javax.ws.rs-api.version>2.0</javax.ws.rs-api.version>
     <mockito-all.version>1.9.5</mockito-all.version>
@@ -34,11 +33,6 @@
   </properties>
 
   <dependencies>
-    <dependency>
-      <groupId>com.fasterxml.jackson.core</groupId>
-      <artifactId>jackson-databind</artifactId>
-      <version>${fasterxml.jackson.version}</version>
-    </dependency>
     <dependency>
       <!-- this is required by OSGi bean declared in src/main/resources-filtered/OSGI-INF/blueprint/beans.xml -->
       <groupId>com.fasterxml.jackson.jaxrs</groupId>
@@ -97,12 +91,6 @@
       <groupId>pentaho-kettle</groupId>
       <artifactId>kettle-ui-swt</artifactId>
       <version>${webspoon.version}</version>
-      <scope>provided</scope>
-    </dependency>
-    <dependency>
-      <groupId>org.eclipse.rap</groupId>
-      <artifactId>org.eclipse.rap.rwt</artifactId>
-      <version>${rap.version}-SNAPSHOT</version>
       <scope>provided</scope>
     </dependency>
     <dependency>

plugins/repositories/core/src/main/java/org/pentaho/di/ui/repo/dialog/RepositoryDialog.java
@@ -25,27 +25,16 @@ package org.pentaho.di.ui.repo.dialog;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.browser.BrowserFunction;
 import org.eclipse.swt.graphics.Image;
-import org.eclipse.swt.widgets.DirectoryDialog;
 import org.eclipse.swt.widgets.Shell;
-import org.json.simple.JSONObject;
-import org.pentaho.di.core.Const;
-import org.pentaho.di.core.database.DatabaseMeta;
-import org.pentaho.di.core.exception.KettleAuthException;
 import org.pentaho.di.core.logging.KettleLogStore;
 import org.pentaho.di.core.logging.LogChannelInterface;
 import org.pentaho.di.i18n.BaseMessages;
 import org.pentaho.di.repository.RepositoryMeta;
-import org.pentaho.di.ui.core.database.dialog.DatabaseDialog;
 import org.pentaho.di.ui.core.dialog.ThinDialog;
 import org.pentaho.di.ui.core.gui.GUIResource;
 import org.pentaho.di.ui.repo.controller.RepositoryConnectController;
-import org.pentaho.di.ui.repo.endpoints.RepositoryEndpoint;
-import org.pentaho.di.ui.repo.model.LoginModel;
-import org.pentaho.di.ui.repo.model.RepositoryModel;
 import org.pentaho.platform.settings.ServerPort;
 import org.pentaho.platform.settings.ServerPortRegistry;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -108,165 +97,6 @@ public class RepositoryDialog extends ThinDialog {
       }
     };
 
-    new BrowserFunction( browser, "bfAddRepository" ) {
-      @Override public Object function( Object[] arguments ) {
-        String json = (String) arguments[0];
-        RepositoryModel model = (RepositoryModel) jsonToObject( json, RepositoryModel.class );
-        if ( controller.createRepository( model.getId(), controller.modelToMap( model ) ) != null ) {
-          return true;
-        } else {
-          return BaseMessages.getString( PKG, "RepositoryConnection.Error.InvalidServer" );
-        }
-      }
-    };
-
-    new BrowserFunction( browser, "bfGetRepositories" ) {
-      @Override public Object function( Object[] arguments ) {
-        return controller.getRepositories();
-      }
-    };
-
-    new BrowserFunction( browser, "bfHelp" ) {
-      @Override public Object function( Object[] objects ) {
-        return controller.help();
-      }
-    };
-
-    new BrowserFunction( browser, "bfUpdate" ) {
-      @Override public Object function( Object[] arguments ) {
-        String json = (String) arguments[0];
-        RepositoryModel model = (RepositoryModel) jsonToObject( json, RepositoryModel.class );
-        JSONObject jsonObject = new JSONObject();
-        if ( controller.updateRepository( model.getId(), controller.modelToMap( model ) ) ) {
-          jsonObject.put( RepositoryConnectController.SUCCESS, true );
-        } else {
-          jsonObject.put( RepositoryConnectController.MESSAGE,
-            BaseMessages.getString( PKG, "RepositoryConnection.Error.InvalidServer" ) );
-          jsonObject.put( RepositoryConnectController.SUCCESS, false );
-        }
-        return jsonObject.toString();
-      }
-    };
-
-    new BrowserFunction( browser, "bfLogin" ) {
-      @Override public Object function( Object[] arguments ) {
-        String json = (String) arguments[0];
-        LoginModel loginModel = (LoginModel) jsonToObject( json, LoginModel.class );
-        JSONObject jsonObject = new JSONObject();
-        try {
-          if ( controller.isRelogin() ) {
-            controller
-              .reconnectToRepository( loginModel.getRepositoryName(), loginModel.getUsername(), loginModel.getPassword() );
-          } else {
-            controller
-              .connectToRepository( loginModel.getRepositoryName(), loginModel.getUsername(), loginModel.getPassword() );
-          }
-          jsonObject.put( RepositoryConnectController.SUCCESS, true );
-        } catch ( Exception e ) {
-          if ( e.getMessage().contains( RepositoryEndpoint.ERROR_401 ) || e instanceof KettleAuthException ) {
-            jsonObject.put( RepositoryConnectController.MESSAGE,
-              BaseMessages.getString( RepositoryEndpoint.class, "RepositoryConnection.Error.InvalidCredentials" ) );
-          } else {
-            jsonObject.put( RepositoryConnectController.MESSAGE,
-              BaseMessages.getString( RepositoryEndpoint.class, "RepositoryConnection.Error.InvalidServer" ) );
-          }
-          jsonObject.put( RepositoryConnectController.SUCCESS, false );
-        }
-        return jsonObject.toString();
-      }
-    };
-
-    new BrowserFunction( browser, "bfGetRepository" ) {
-      @Override public Object function( Object[] objects ) {
-        return controller.getRepository( (String) objects[ 0 ] );
-      }
-    };
-
-    new BrowserFunction( browser, "bfCheckDuplicate" ) {
-      @Override public Object function( Object[] arguments ) {
-        String displayName = (String) arguments[0];
-        return controller.checkDuplicate( displayName );
-      }
-    };
-
-    new BrowserFunction( browser, "bfGetDatabases" ) {
-      @Override public Object function( Object[] arguments ) {
-        return controller.getDatabases();
-      }
-    };
-
-    new BrowserFunction( browser, "bfCreateNewConnection" ) {
-      @Override public Object function( Object[] arguments ) {
-        DatabaseDialog databaseDialog = new DatabaseDialog( shell, new DatabaseMeta() );
-        databaseDialog.open();
-        DatabaseMeta databaseMeta = databaseDialog.getDatabaseMeta();
-        JSONObject jsonObject = new JSONObject();
-        if ( databaseMeta != null ) {
-          if ( !controller.isDatabaseWithNameExist( databaseMeta, true ) ) {
-            controller.addDatabase( databaseMeta );
-          } else {
-            DatabaseDialog.showDatabaseExistsDialog( shell, databaseMeta );
-          }
-          jsonObject.put( "name", databaseMeta.getName() );
-        } else {
-          jsonObject.put( "name", "None" );
-        }
-        return jsonObject.toJSONString();
-      }
-    };
-
-    new BrowserFunction( browser, "bfEditConnection" ) {
-      @Override public Object function( Object[] arguments ) {
-        String database = (String) arguments[0];
-        DatabaseMeta databaseMeta = controller.getDatabase( database );
-        String originalName = databaseMeta.getName();
-        DatabaseDialog databaseDialog = new DatabaseDialog( shell, databaseMeta );
-        databaseDialog.open();
-        JSONObject jsonObject = new JSONObject();
-        if ( !controller.isDatabaseWithNameExist( databaseMeta, false ) ) {
-          controller.save();
-          jsonObject.put( "name", databaseMeta.getName() );
-        } else {
-          DatabaseDialog.showDatabaseExistsDialog( shell, databaseMeta );
-          databaseMeta.setName( originalName );
-          databaseMeta.setDisplayName( originalName );
-          jsonObject.put( "name", originalName );
-        }
-        return jsonObject.toJSONString();
-      }
-    };
-
-    new BrowserFunction( browser, "bfDeleteConnection" ) {
-      @Override public Object function( Object[] arguments ) {
-        String database = (String) arguments[0];
-        controller.removeDatabase( database );
-        return true;
-      }
-    };
-
-    new BrowserFunction( browser, "bfRemove" ) {
-      @Override public Object function( Object[] arguments ) {
-        String displayName = (String) arguments[0];
-        return controller.deleteRepository( displayName );
-      }
-    };
-
-    new BrowserFunction( browser, "bfBrowse" ) {
-      @Override public Object function( Object[] objects ) {
-        DirectoryDialog directoryDialog = new DirectoryDialog( shell );
-        String location = directoryDialog.open();
-        if ( location == null ) {
-          location = "";
-        }
-        browser.evaluate(
-          "var location = document.getElementById( 'location' );"
-          + "var scope = angular.element( location ).scope( 'file' );"
-          + String.format( "scope.$apply(function(){ scope.vm.connection.location = '%s';});", location )
-        );
-        return "";
-      }
-    };
-
     controller.setCurrentRepository( repositoryMeta );
     controller.setRelogin( relogin );
 
@@ -325,17 +155,5 @@ public class RepositoryDialog extends ThinDialog {
 
   private static String getRepoURL( String path ) {
     return System.getProperty( "KETTLE_CONTEXT_PATH", "" ) + "/osgi" + getClientPath() + path;
-  }
-
-  private static Object jsonToObject( String json, Class<?> _class ) {
-    ObjectMapper mapper = new ObjectMapper();
-    Object model = null;
-    try {
-      model = mapper.readValue( json, _class );
-    } catch ( Exception e ) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    }
-    return model;
   }
 }

plugins/repositories/core/src/main/java/org/pentaho/di/ui/repo/endpoints/WebSpoonFilter.java
@@ -1,0 +1,47 @@
+/*! ******************************************************************************
+ *
+ * Pentaho Data Integration
+ *
+ * Copyright (C) 2002-2019 by Hitachi Vantara : http://www.pentaho.com
+ * Copyright (C) 2016-2019 by Hitachi America, Ltd., R&D : http://www.hitachi-america.us/rd/
+ *
+ *******************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************/
+
+package org.pentaho.di.ui.repo.endpoints;
+
+import java.io.IOException;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.ext.Provider;
+
+import org.eclipse.rap.rwt.service.UISession;
+import org.pentaho.di.ui.spoon.WebSpoonUtils;
+
+@Provider
+public class WebSpoonFilter implements ContainerRequestFilter {
+
+  @Override
+  public void filter( ContainerRequestContext requestContext ) throws IOException {
+    MultivaluedMap<String, String> params = requestContext.getUriInfo().getQueryParameters();
+    String cid = params.getFirst( "cid" );
+    UISession uiSession = WebSpoonUtils.getUISession( cid );
+    WebSpoonUtils.setUISession( uiSession );
+  }
+
+}

plugins/repositories/core/src/main/javascript/app/components/database/details/details.service.js
@@ -55,7 +55,7 @@ define(
          * @returns {Promise} - a promise to be resolve when the server returns
          */
         function checkDuplicate(repository) {
-          return helperService.async( bfCheckDuplicate( repository.displayName ) );
+          return helperService.httpPost([baseUrl, "duplicate"].join("/"), repository);
         }
       }
     });

plugins/repositories/core/src/main/javascript/app/components/database/select/select.service.js
@@ -58,7 +58,7 @@ define(
          * @returns {Promise}
          */
         function getDatabases(repository) {
-          return helperService.async( bfGetDatabases() );
+          return helperService.httpGet([baseUrl, "databases"].join("/"), repository);
         }
 
         /**
@@ -67,7 +67,7 @@ define(
          * @returns {Promise}
          */
         function createNewConnection() {
-          return helperService.async( bfCreateNewConnection() );
+          return helperService.httpGet([baseUrl, "connection", "create"].join("/"));
         }
 
         /**
@@ -77,7 +77,7 @@ define(
          * @returns {Promise}
          */
         function editConnection(database) {
-          return helperService.async( bfEditConnection( database ) );
+          return helperService.httpPost([baseUrl, "connection", "edit"].join("/"), database);
         }
 
         /**
@@ -87,7 +87,7 @@ define(
          * @returns {Promise}
          */
         function deleteConnection(database) {
-          return helperService.async( bfDeleteConnection( database ) );
+          return helperService.httpPost([baseUrl, "connection", "delete"].join("/"), database);
         }
       }
     });

plugins/repositories/core/src/main/javascript/app/components/file/details/details.service.js
@@ -50,12 +50,11 @@ define(
         };
 
         function checkDuplicate(repository) {
-          return helperService.async( bfCheckDuplicate( repository.displayName ) );
+          return helperService.httpPost([baseUrl, "duplicate"].join("/"), repository);
         }
 
         function browse() {
-          bfBrowse();
-          return helperService.async( "{ \"path\": \"dummy\" }" );
+          return helperService.httpGet([baseUrl, "browse"].join("/"));
         }
       }
     });

plugins/repositories/core/src/main/javascript/app/components/file/loading/loading.service.js
@@ -50,11 +50,11 @@ define(
         };
 
         function addRepository(repository) {
-          return helperService.async( bfAddRepository( JSON.stringify( repository ) ) );
+          return helperService.httpPost([baseUrl, "add"].join("/"), repository);
         }
 
         function updateRepository(repository) {
-          return helperService.async( bfUpdate( JSON.stringify( repository ) ) );
+          return helperService.httpPost([baseUrl, "update"].join("/"), repository);
         }
       }
     });

commit aa25ebb49176a5e464cf61879f6046eb893651d5

ServerPush upon ThinDialog.dispose() has never been required (fix #148)
This fixes a regression caused by a55b60e9.

ui/src/main/java/org/pentaho/di/ui/core/dialog/ThinDialog.java
@@ -22,14 +22,11 @@
 
 package org.pentaho.di.ui.core.dialog;
 
-import org.eclipse.rap.rwt.service.ServerPushSession;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.browser.Browser;
 import org.eclipse.swt.browser.BrowserFunction;
 import org.eclipse.swt.browser.CloseWindowListener;
 import org.eclipse.swt.browser.WindowEvent;
-import org.eclipse.swt.events.DisposeEvent;
-import org.eclipse.swt.events.DisposeListener;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.graphics.Rectangle;
@@ -93,11 +90,6 @@ public class ThinDialog extends Dialog {
       messageBox.open();
     }
     setPosition();
-    final ServerPushSession pushSession = new ServerPushSession();
-    pushSession.start();
-    dialog.addDisposeListener( ( event ) -> {
-      pushSession.stop();
-    });
     dialog.open();
   }
 